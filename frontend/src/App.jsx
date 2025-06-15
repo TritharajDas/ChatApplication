f322b284c9799dca1fd4b6fcd865c556a31f262c
TirtharajDas <tirtharaj.soft@gmail.com>
2025-06-15 09:11:20 +0530

    Tailwind and daisyUI integrated properly

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,23 @@
-import React from 'react'
+import Navbar from './components/Navbar'
 import { Routes, Route } from 'react-router-dom'
 import HomePage from './pages/HomePage'
 import LoginPage from './pages/LoginPage'
 import SignUpPage from './pages/SignUpPage'
 import SettingsPage from './pages/SettingsPage'
 import ProfilePage from './pages/ProfilePage'
-import Navbar from './components/Navbar'
 
- 
 const App = () => {
   return (
     <div>
-      <Navbar/>
-      <button className="btn btn-neutral">Neutral</button>
-      <button className="btn btn-primary">Primary</button>
-      <button className="btn btn-secondary">Secondary</button>
-      <button className="btn btn-accent">Accent</button>
-      <button className="btn btn-info">Info</button>
-      <button className="btn btn-success">Success</button>
-      <button className="btn btn-warning">Warning</button>
-<button className="btn btn-error">Error</button>
+      <Navbar />
 
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignUpPage />} />
         <Route path="/settings" element={<SettingsPage />} />
-        <Route path="/profile" element={<ProfilePage />} />
+        <Route path="/profile/:id" element={<ProfilePage />} />
       </Routes>
-
-
     </div>
   )
 }
