d60b63bbc4449c3eaee4525dd6c3282d08766402
TirtharajDas <tirtharaj.soft@gmail.com>
2025-07-06 03:44:19 +0530

    production ready

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,10 +7,15 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 import { app, server } from "./lib/socket.js";
 
+
+import path from "path";
+
+
 dotenv.config();
 
 
 const PORT = process.env.PORT;
+const __dirname = path.resolve();
 
 // Middleware to parse JSON request bodies
 app.use(express.json({ limit: '10mb' }));           // ← increase limit here
@@ -30,6 +35,14 @@ app.use(cors({
 
 app.use("/api/auth", authRoute);
 app.use("/api/messages", messageRoute);
+
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname,"../frontend", "dist", "index.html"));
+    });
+}
 
 server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
